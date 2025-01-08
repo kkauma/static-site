43a4baa59e005aca4860a1011a83c36a2f118088
kkauma <kylekauma@gmail.com>
2025-01-07 17:47:54 -0700

    Adjust styles and fix navbar

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const menuButton = document.querySelector(".menu-button");
   const navLinks = document.querySelector(".nav-links");
+  const nav = document.querySelector("nav");
 
   menuButton.addEventListener("click", () => {
     navLinks.classList.toggle("active");
@@ -10,6 +11,14 @@ document.addEventListener("DOMContentLoaded", () => {
   document.addEventListener("click", (e) => {
     if (!e.target.closest(".nav-container")) {
       navLinks.classList.remove("active");
+    }
+  });
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 20) {
+      nav.classList.add("scrolled");
+    } else {
+      nav.classList.remove("scrolled");
     }
   });
 });
